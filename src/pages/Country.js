bfe7e7d1ca0045971ac668b6484028121c309cbe
Emma Ann Hynes <ekiizu2003@gmail.com>
2024-10-30 12:17:28 +0000

    added bootstrap

---
+++ src/pages/Country.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Image, Row, Col } from "react-bootstrap";
+
+const Country = () => {
+
+  const { name } = useParams();
+
+  // Store the country data in a state
+  const [country, setCountry] = useState({});
+
+  // We could make a second API request here to get more details about the country
+  useEffect(() => {
+    axios
+      .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then((response) => {
+        setCountry(response.data[0]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  if (!country.name || !country.flags) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <Row>
+        <Col>
+          <Image
+            src={country.flags?.png}
+            alt={`${country.name?.common}'s flag`}
+          />
+        </Col>
+        <Col>
+          <p>
+            <b>Common Name:</b> {country.name.common}
+          </p>
+          <p>
+            <b>Official Name:</b> {country.name.official}
+          </p>
+          <p>
+            <b>Region:</b> {country.region}
+          </p>
+          <p>
+            <b>Sub Region:</b> {country.subregion}
+          </p>
+          <p>
+            <b>Currency:</b> {Object.values(country.currencies)[0].name}
+          </p>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Country;
